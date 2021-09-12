1b3d3625e6211eed4649b23b6284cb129fac4c25
adigun.mubaraq06 <adigun.mubaraq06@gmail.com>
2021-09-12 19:04:18 +0100

    Added section to view collections

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,10 @@
 import { Box, Button, Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import { AuthenticIcon, CustomerServiceIcon, DeliveryIcon } from "../assets/svg";
+import BeautifulClock from '../assets/images/collections/image_one.png'
+import BeautifulSofa from '../assets/images/collections/image_two.png'
+import BeautifulLamp from '../assets/images/collections/image_three.png'
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "center"
     }
 }));
+
 
 export default function Project() {
     const classes = useStyles();
@@ -81,9 +86,20 @@ export default function Project() {
                     </Box>
                 </Grid>
             </Grid>
-            <Grid className={classes.container}>
+            <Grid className="container">
                 <p className="collection-title">Inspiration Collection</p>
                 <p className="collection-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <Grid container justifyContent="space-evenly" mb={3}>
+                    <Box mt={5}>
+                        <img src={BeautifulClock} alt="beautifulclock" />
+                    </Box>
+                    <Box>
+                        <img src={BeautifulSofa} alt="beautifulSofa"/>
+                    </Box>
+                    <Box mt={5}>
+                        <img src={BeautifulLamp} alt="beautifulLamp" />
+                    </Box>
+                </Grid>
             </Grid>
         </div>
     );
